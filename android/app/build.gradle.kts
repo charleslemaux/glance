eeca28c21742f8822c6377f98cb2fc539d1567fe
charleslemaux <charles@le-maux.dev>
2025-04-16 21:08:44 +0200

    [!] Updated dependencies for cross compilations

--- android/app/build.gradle.kts
+++ android/app/build.gradle.kts
@@ -8,7 +8,7 @@ plugins {
 android {
     namespace = "com.example.glance"
     compileSdk = flutter.compileSdkVersion
-    ndkVersion = flutter.ndkVersion
+    ndkVersion = "27.0.12077973"
 
     compileOptions {
         sourceCompatibility = JavaVersion.VERSION_11
